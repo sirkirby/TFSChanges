7aa05718884b4c569d875fd539aec3abc369e329
Chris Kirby <chrisk@ryantechinc.com>
2013-07-22 18:32:27 -0700

    initial commit
    Settings aren't quite done.. going to use azure blob storage instead of
    location storage due to permissions

---
+++ TFSChanges/Preferences.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net.Mime;
+using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
+using Newtonsoft.Json;
+
+namespace TFSChanges
+{
+	[JsonObject]
+	public class Preferences
+	{
+		public string Path
+		{
+			get
+			{
+				return System.IO.Path.GetDirectoryName(new Uri(Assembly.GetCallingAssembly().CodeBase).LocalPath);
+			}
+		}
+
+		public DateTime LastChecked { get; set; }
+
+		public Preferences()
+		{
+			var path = Path;
+			if (File.Exists(path))
+			{
+				var settings = JsonConvert.DeserializeObject<Preferences>(File.ReadAllText(path));
+				LastChecked = settings.LastChecked;
+			}
+			else
+			{
+				LastChecked = DateTime.UtcNow.AddHours(-12);
+				Save();
+			}
+		}
+
+		public void Save()
+		{
+			File.WriteAllText(Path, JsonConvert.SerializeObject(this));
+		}
+	}
+}
