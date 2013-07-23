da99d394d05abb7c70d8f1645604e7aeec12af1b
Chris Kirby <chrisk@ryantechinc.com>
2013-07-23 09:29:07 -0700

    converted preferences to use azure storage instead of the file system. replace gac odata aseembly with nuget one. some other code cleanup and comments for readability.

--- TFSChanges/Preferences.cs
+++ TFSChanges/Preferences.cs
@@ -1,44 +1,57 @@
 ﻿using System;
-using System.IO;
-using System.Net.Mime;
 using System.Reflection;
-using System.Security;
-using System.Security.Permissions;
-using Newtonsoft.Json;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using TFSChanges.Properties;
 
 namespace TFSChanges
 {
-	[JsonObject]
-	public class Preferences
+	public class Preferences : TableEntity
 	{
-		public string Path
-		{
-			get
-			{
-				return System.IO.Path.GetDirectoryName(new Uri(Assembly.GetCallingAssembly().CodeBase).LocalPath);
-			}
-		}
+		private CloudTable _table;
 
 		public DateTime LastChecked { get; set; }
 
+		/// <summary>
+		/// Init the preferences
+		/// </summary>
 		public Preferences()
 		{
-			var path = Path;
-			if (File.Exists(path))
-			{
-				var settings = JsonConvert.DeserializeObject<Preferences>(File.ReadAllText(path));
-				LastChecked = settings.LastChecked;
-			}
+			LastChecked = DateTime.UtcNow.AddHours(-24);
+		}
+
+		public void Load()
+		{
+			PartitionKey = "TFSChanges";
+			var version = Assembly.GetExecutingAssembly().GetName().Version;
+			RowKey = version.ToString();
+
+			// Retrieve storage account from connection string
+			var storageAccount = CloudStorageAccount.Parse(Settings.Default.RTDevStorage);
+			// Create the table client
+			var tableClient = storageAccount.CreateCloudTableClient();
+			//Create the CloudTable that represents the "people" table.
+			_table = tableClient.GetTableReference(Settings.Default.PreferencesTable);
+
+			var operation = TableOperation.Retrieve<Preferences>(PartitionKey, RowKey);
+			var results = _table.Execute(operation);
+
+			if (results != null)
+				LastChecked = ((Preferences)results.Result).LastChecked;
 			else
 			{
-				LastChecked = DateTime.UtcNow.AddHours(-12);
+				LastChecked = DateTime.UtcNow.AddHours(-24);
 				Save();
 			}
 		}
 
+		/// <summary>
+		/// save the preferences
+		/// </summary>
 		public void Save()
 		{
-			File.WriteAllText(Path, JsonConvert.SerializeObject(this));
+			var operation = TableOperation.InsertOrReplace(this);
+			_table.Execute(operation);
 		}
 	}
 }
