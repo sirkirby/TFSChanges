015ad43392d41605fdb1fcea263847944359f061
Chris Kirby <chris@infinitusdev.com>
2013-11-21 13:40:54 -0800

    complete overhaul as i prep for open sourcing the project. abstracted all program.cs code out in to a basic provider model where output/post providers are dynamically loaded and executed. each new provider implementeation is essentially an output channel for the collected data from the tfs odata api. preference were completly rerewritted with new asyc api and just cleaner overall. nearly all settings have been removed with just one more private setting to go. settings now belong to the preferences table storage. added automapper to reduce code. Added new url properties to project configuration. these new urls are used to enhance the output messages even further by allowing end users to click directly to the notification source. I've also attempted to abstract out any other proprietary and hard coded items such as youtrack and tfs data.

---
+++ TFSChanges/ProviderAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TFSChanges
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+	public class ProviderAttribute : Attribute
+	{
+		/// <summary>
+		/// Gets or sets the name.
+		/// </summary>
+		/// <value>The name.</value>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ProviderAttribute"/> class.
+		/// </summary>
+		/// <param name="name">The name.</param>
+		public ProviderAttribute(string name)
+		{
+			Name = name;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ProviderAttribute"/> class.
+		/// </summary>
+		public ProviderAttribute() : this ("Provider") {}
+	}
+}
